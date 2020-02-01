2e2ef71fadf50320aa3f5e2f63bca5375d6a6b4e
Joshua Nixon <nixonjoshua98@gmail.com>
2020-02-01 13:56:11 +0000

    Added boxes

---
+++ Assets/Scripts/CoinBox.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinBox : MonoBehaviour
+{
+	CollisionTrigger colTrigger;
+
+	public Sprite afterHitSprite;
+
+	bool hasBeenHit = false;
+
+	private void Awake()
+	{
+		colTrigger = GetComponentInChildren<CollisionTrigger>();
+	}
+
+	private void Update()
+	{
+		if (hasBeenHit) return;
+
+
+		if (colTrigger.isCollided)
+		{
+			OnHit();
+		}
+	}
+
+	void OnHit()
+	{
+		hasBeenHit = true;
+
+		GetComponent<SpriteRenderer>().sprite = afterHitSprite;
+	}
+}
---
+++ Assets/Scripts/CollisionTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionTrigger : MonoBehaviour
+{
+	public bool isCollided = false;
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (collision.gameObject.CompareTag("Player"))
+		{
+			isCollided = true;
+		}
+	}
+}
