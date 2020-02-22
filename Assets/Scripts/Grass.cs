96c8805ef4c1376071f90e81e983282419ddbc8a
Joshua Nixon <nixonjoshua98@gmail.com>
2020-02-22 16:08:26 +0000

    Added

---
+++ Assets/Scripts/Grass.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grass : MonoBehaviour
+{
+	[Header("Sprites")]
+	public Sprite square;
+
+	// Components
+	SpriteRenderer spriteRenderer;
+
+	private void Start()
+	{
+		spriteRenderer = GetComponent<SpriteRenderer>();
+
+		if (GameManager.instance.assetMode == AssetMode.PRIMITIVE)
+		{
+			spriteRenderer.sprite = square;
+
+			spriteRenderer.color = new Color(0.0f, 1.0f, 0.0f);
+		}
+	}
+}
---
+++ Assets/Scripts/GridBackground.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridBackground : MonoBehaviour
+{
+	// Start is called before the first frame update
+	void Start()
+	{
+		if (GameManager.instance.assetMode == AssetMode.PRIMITIVE)
+		{
+			Destroy(gameObject);
+		}
+
+	}
+}
